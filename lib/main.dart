a0c3b5ca3a6e6c720763cd24db4c033b86a6071d
Aymeric <lefeyer.aymeric@gmail.com>
2019-09-18 23:47:13 +0200

    Page d'accueil v1 Entraînement sur les liens Ajout du package url_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,28 @@
+import 'package:bonap/presentation/custom_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Compteur'),
+      home: MyHomePage(title: 'Bonap\''),
     );
   }
+
+
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,134 +31,100 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void _resetCounter()
-  {
-    setState(() {
-      _counter = 0;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
 
           children: <Widget>[
 
-            Text(
-              'Valeur actuelle du compteur:',
-            ),
-
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-
-
-
-
-
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-
-                FloatingActionButton(
-                  onPressed: _decrementCounter,
-                  tooltip: 'Diminuer',
-                  child: Icon(Icons.remove),
-                ),
-
-                FlatButton(
-                  color: Colors.blue,
-                  textColor: Colors.white,
-
-                  onPressed: () {
-                    _resetCounter();
-                  },
-
-                  child: Text(
-                    "Reset",
-                  ),
-                ),
-
-                FloatingActionButton(
-                  onPressed: _incrementCounter,
-                  tooltip: 'Augmenter',
-                  child: Icon(Icons.add),
-                ),
-              ],
-
-            ),
-
 
             const Image(
-              image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+              alignment: Alignment.topCenter,
+              image: AssetImage('assets/logo_bonap.png'),
+            ),
 
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  'CARRY Quentin - LE FEYER Aymeric',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Row(
+
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      FloatingActionButton(
+                        child: Icon(Custom.github_circled),
+                        onPressed: _launchURLGit,
+                        tooltip: 'Git',
+
+                      ),
+
+                      FloatingActionButton(
+                        child: Icon(Custom.trello),
+                        onPressed: _launchURLTrello,
+                        tooltip: 'Trello',
+
+                      )
+                    ],
+
+                  ),
+                ),
+                Text(
+                  'Bonap est un gestionnaire de menu intelligent \n'
+                      'Ajoutez des recettes et commencez votre menu !\n'
+                      'La liste de course sera automatiquement générée\n'
+                      'Une estimation du prix sera alors générée\n'
+                      'Membre d\'une famille ? Ajoutez-la !\n'
+                      '\n'
+                      'Tout est prêt ? Dans ce cas, Bonap !',
+                  style: TextStyle(fontSize: 16.0),
+                )
+
+
+              ],
             )
 
 
-
-
-
           ],
-
         ),
-
-
-
 
       ),
 
 
-
-
-
-
-
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
-
   }
+
+  void _launchURLGit() async {
+    const url = 'https://github.com/AymericLeFeyer/Bonap';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _launchURLTrello() async {
+    const url = 'https://trello.com/b/Vi01l5l7';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
 }
