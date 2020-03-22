5d8eeb86d23c3bbbf2c62530a44fe5e66f5494d8
Stelario <55248476+Stelario@users.noreply.github.com>
2020-03-22 18:22:35 +0100

    FeedBack v1

--- lib/files/drawerItems/feedback.dart
+++ lib/files/drawerItems/feedback.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:bonap/files/tools.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FeedbackReport extends StatefulWidget {
   @override
@@ -7,6 +12,41 @@ class FeedbackReport extends StatefulWidget {
 }
 
 class _FeedbackReportState extends State<FeedbackReport> {
+ List<String> attachments = [];
+  bool isHTML = false;
+
+  final _subjectController = TextEditingController(text: 'The subject');
+
+  final _bodyController = TextEditingController(
+    text: 'Mail body.',
+  );
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future<void> send() async {
+    final Email email = Email(
+      body: _bodyController.text,
+      subject: _subjectController.text,
+      recipients: ["stelapix.bonap@gmail.com"],
+      attachmentPaths: attachments,
+    );
+
+    String platformResponse;
+
+    try {
+      await FlutterEmailSender.send(email);
+      platformResponse = 'success';
+    } catch (error) {
+      platformResponse = error.toString();
+    }
+
+    if (!mounted) return;
+
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text(platformResponse),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,12 +63,62 @@ class _FeedbackReportState extends State<FeedbackReport> {
                   end: Alignment.bottomRight,
                   colors: <Color>[OwnColor.yellowLogo, OwnColor.blueLogo])),
         ),
+        actions: <Widget>[
+            IconButton(
+              onPressed: send,
+              icon: Icon(Icons.send),
+            )
+          ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-            "Cette application est une beta, pourriez vous s'il vous plait envoyez vos suggestions ou des bugs que vous auriez trouvé à l'adresse : Stelapix.bonap@gmail.com"),
-      ),
+      body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _subjectController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Subject',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _bodyController,
+                    maxLines: 10,
+                    decoration: InputDecoration(
+                        labelText: 'Body', border: OutlineInputBorder()),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          icon: Icon(Icons.camera),
+          label: Text('Add Image'),
+          onPressed: _openImagePicker,
+        ),
+      
+      // body: Padding(
+      //   padding: const EdgeInsets.all(20),
+      //   child: Text(
+      //       "Cette application est une beta, pourriez vous s'il vous plait envoyez vos suggestions ou des bugs que vous auriez trouvé à l'adresse : Stelapix.bonap@gmail.com"),
+      // ),
     );
+  }
+
+  void _openImagePicker() async {
+    File pick = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      attachments.add(pick.path);
+    });
   }
 }
